ae4c947055373d05e62a032bd966b8d07a4e5dd8
arjunlaxane <arjunlaxane@gmail.com>
2022-12-23 21:05:21 +0530

    commit

---
+++ src/Global.js
@@ -0,0 +1,1 @@
+export const API = 'http://192.168.29.223:4011';
---
+++ src/component/UserProfile.js
@@ -0,0 +1,67 @@
+import React, { useEffect, useState } from 'react';
+import { useNavigate } from 'react-router-dom';
+import './Userprofile.css';
+import { API } from '../Global';
+
+const UserProfile = () => {
+  const [userData, setUserData] = useState([]);
+
+  const navigate = useNavigate();
+
+  const getUserDetails = () => {
+    fetch(`${API}/me`)
+      .then(res => res.json())
+      .then(data => setUserData(data));
+  };
+
+  useEffect(() => {
+    getUserDetails();
+  }, [userData]);
+
+  const handleLogout = () => {
+    fetch(`${API}/logout`);
+    alert('Log out successful');
+    navigate('/login');
+  };
+
+  return (
+    <>
+      {userData && (
+        <div className="user-profile">
+          <div className="profile_container">
+            <div className="info">
+              <h3>Name :</h3>
+              <span>{userData?.userDetail?.name}</span>
+            </div>
+            <div className="info">
+              <h3>Email :</h3>
+              <span>{userData?.userDetail?.email}</span>
+            </div>
+            <div className="info">
+              <h3>Age :</h3>
+              <span>{userData?.userDetail?.age}</span>
+            </div>
+            <div className="info">
+              <h3>Gender :</h3>
+              <span>{userData?.userDetail?.gender}</span>
+            </div>
+            <div className="info">
+              <h3>DOB :</h3>
+              <span>{userData?.userDetail?.dob}</span>
+            </div>
+            <div className="info">
+              <h3>Mobile :</h3>
+              <span>{userData?.userDetail?.mobile}</span>
+            </div>
+          </div>
+          <button id="update" onClick={() => navigate('/me/update')}>
+            Update Profile
+          </button>
+          <button onClick={handleLogout}>Logout</button>
+        </div>
+      )}
+    </>
+  );
+};
+
+export default UserProfile;
